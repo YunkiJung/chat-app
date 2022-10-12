0ff8871ef26ca23fd4fc187061a101f1f488cd3c
YunkiJung <100543183+YunkiJung@users.noreply.github.com>
2022-10-11 21:06:44 -0400

    bcrypt delete

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 const User = require("../models/userModel");
-const bcrypt = require("bcrypt");
+//const bcrypt = require("bcrypt");
 
 module.exports.login = async (req, res, next) => {
   try {
@@ -7,8 +7,8 @@ module.exports.login = async (req, res, next) => {
     const user = await User.findOne({ username });
     if (!user)
       return res.json({ msg: "Incorrect Username or Password", status: false });
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid)
+    //const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (user.password != password)
       return res.json({ msg: "Incorrect Username or Password", status: false });
     delete user.password;
     return res.json({ status: true, user });
@@ -26,11 +26,11 @@ module.exports.register = async (req, res, next) => {
     const emailCheck = await User.findOne({ email });
     if (emailCheck)
       return res.json({ msg: "Email already used", status: false });
-    const hashedPassword = await bcrypt.hash(password, 10);
+    //const hashedPassword = await bcrypt.hash(password, 10);
     const user = await User.create({
       email,
       username,
-      password: hashedPassword,
+      password: password,
     });
     delete user.password;
     return res.json({ status: true, user });
