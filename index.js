9f28494e251edb99dbf6bee698d89c73470ea0f8
YunkiJung <100543183+YunkiJung@users.noreply.github.com>
2022-10-11 20:48:03 -0400

    config added

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const authRoutes = require("./routes/auth");
 const messageRoutes = require("./routes/messages");
 const app = express();
 const socket = require("socket.io");
-require("dotenv").config();
+const MONGO_URL = require("./config.js");
+//require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect(MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -26,7 +27,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 
-const server = app.listen(process.env.PORT, () =>
+const server = app.listen(process.env.PORT || '3000', () =>
   console.log(`Server started on ${process.env.PORT}`)
 );
 const io = socket(server, {
